9af47cd22cc84560387e93b9c51b45e60df4091a
aar <andrei.arion@lesfurets.com>
2022-11-25 10:17:32 +0100

    Working example polynote/spark

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
+SPARK_VERSION=3.3.1
 
+set -e
 docker compose -f "docker-compose.yml" down
-docker build --pull --rm -f "inf728-cassandra-spark/Dockerfile" -t inf728-cassandra-spark:3.0.2 "inf728-cassandra-spark"
+docker build --pull --rm -f "inf728-cassandra-spark/Dockerfile" -t inf728-cassandra-spark:${SPARK_VERSION} "inf728-cassandra-spark"
 docker build --pull --rm -f "inf728-polynote/Dockerfile" -t inf728-polynote:0.4.10 "inf728-polynote"
 docker compose -f "docker-compose.yml" up -d --build
